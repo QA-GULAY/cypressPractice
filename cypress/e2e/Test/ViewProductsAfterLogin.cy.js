0db75f3998f1f480b5871ffc6814c2f2d9f8565c
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-13 23:24:59 +0100

    Update ViewProductsAfterLogin.cy.js

--- cypress/e2e/Test/ViewProductsAfterLogin.cy.js
+++ cypress/e2e/Test/ViewProductsAfterLogin.cy.js
@@ -2,8 +2,8 @@ import HomePage from "../Page/HomePage"
 import LoginPage from "../Page/LoginPage"
 import LOCATORS from "../../support/locators"
 describe("User Can See Added Products After Login",() => {
-    const hmPage = new HomePage()
-    const lgnPage = new LoginPage()
+    const homePage = new HomePage()
+    const loginPage = new LoginPage()
     let user;
     before(() => {
         cy.fixture('userDatas/info').then((userInfo) => {
@@ -11,7 +11,7 @@ describe("User Can See Added Products After Login",() => {
         })
     })
  it("User Can See Added Product After Login" , () => {
-        hmPage.visitPage()
+        homePage.visitPage()
         cy.getBySel(LOCATORS.HOME_PAGE.HEADER).should("be.visible");
         cy.getBySel(LOCATORS.PRODUCT_PAGE.PRODUCT_BTN).click().wait(2000);
         cy.getBySel(LOCATORS.PRODUCT_PAGE.ALL_PRODUCTS_TEXT).should('be.visible');
@@ -21,10 +21,10 @@ describe("User Can See Added Products After Login",() => {
         cy.getBySel(LOCATORS.PRODUCT_PAGE.SEARCHED_PRODUCTS_TEXT).should('be.visible')
         cy.getBySel(LOCATORS.ADD_TO_CART.PRODUCT_2).contains('Add to cart').should("be.visible")
         cy.getBySel(LOCATORS.ADD_TO_CART.PRODUCT_2).contains('Add to cart').click().wait(2000)
-        cy.getBySel(LOCATORS.ADD_TO_CART.WIEW_CART).click()
+        cy.getBySel(LOCATORS.ADD_TO_CART.VIEW_CART).click()
         cy.getBySel(LOCATORS.CART_PAGE.CART_DESCRIPTION).should('contain','Men Tshirt')
         cy.getBySel(LOCATORS.CART_PAGE.LOGIN_BTN_CART_PAGE).click()
-        lgnPage.userLogin(user)
+        loginPage.userLogin(user)
         cy.getByCompoundSel(LOCATORS.HOME_PAGE.HEADER,LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should('be.visible')
         cy.getBySel(LOCATORS.HOME_PAGE.CART_BUTTON).click()
         cy.getBySel(LOCATORS.CART_PAGE.CART_DESCRIPTION).should('contain','Men Tshirt')
