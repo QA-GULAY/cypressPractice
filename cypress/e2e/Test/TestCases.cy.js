0dd9b458fdf043094314d274ee6d61cb3898a91c
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-13 21:23:12 +0100

    Update TestCases.cy.js

--- cypress/e2e/Test/TestCases.cy.js
+++ cypress/e2e/Test/TestCases.cy.js
@@ -9,8 +9,7 @@ describe("TestCases Tests", () => {
             user = userInfo
         })
     })
-
-    it("Kullanici test case sayfasina basariyla erisebilmeli", () => {
+ it("Kullanici test case sayfasina basariyla erisebilmeli", () => {
         homePage.visitPage()
         cy.contains(user.home.homePageText).should("be.visible")
         cy.getBySel(LOCATORS.HOME_PAGE.TEST_CASES_BTN).click()
