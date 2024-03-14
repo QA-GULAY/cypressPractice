247cde7acec4ba3b5dbf4bc0187322638a5344d0
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-14 22:47:00 +0100

    Update LogoutUserTest.cy.js

--- cypress/e2e/Test/LogoutUserTest.cy.js
+++ cypress/e2e/Test/LogoutUserTest.cy.js
@@ -2,8 +2,9 @@ import HomePage from "../Page/HomePage"
 import LoginPage from "../Page/LoginPage"
 import LOCATORS from "../../support/locators"
 describe('Kullanici siteye kaydini sildiginde giris sayfasina geri gelebilmeli',()=>{
-    const homPage = new HomePage()
-    const lginPage = new LoginPage()
+    
+    const homePage = new HomePage()
+    const loginPage = new LoginPage()
     let user;
     before(() => {
         cy.fixture('userDatas/info').then((userInfo) => {
@@ -11,13 +12,13 @@ describe('Kullanici siteye kaydini sildiginde giris sayfasina geri gelebilmeli',
         })
     })
     it('Kullanici siteye kaydini sildiginde giris sayfasina geri gelebilmeli',()=>{
-homPage.visitPage()
-cy.getBySel(LOCATORS.HOME_PAGE.HEADER).should('be.visible')
-cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
-lginPage.userLogin(user)
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should('be.visible')
-cy.getBySel(LOCATORS.HOME_PAGE.LOGOUT_BTN).click()
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
+        homePage.visitPage()
+        cy.getBySel(LOCATORS.HOME_PAGE.HEADER).should('be.visible')
+        cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
+        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
+        loginPage.userLogin(user)
+        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should('be.visible')
+        cy.getBySel(LOCATORS.HOME_PAGE.LOGOUT_BTN).click()
+        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
      })
 }) 
