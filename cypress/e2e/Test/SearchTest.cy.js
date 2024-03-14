f59c65f50ad23889d069f948719597593c582ccf
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-14 23:05:00 +0100

    Update SearchTest.cy.js

--- cypress/e2e/Test/SearchTest.cy.js
+++ cypress/e2e/Test/SearchTest.cy.js
@@ -1,9 +1,7 @@
 import LOCATORS from "../../support/locators";
 import HomePage from "../Page/HomePage"
-import ProductPage from "../Page/ProductPage";
-
-
 describe("Homepage Tests", () => {
+    
     const homePage = new HomePage()
     let user;
     before(() => {
@@ -11,23 +9,9 @@ describe("Homepage Tests", () => {
             user = userInfo
         })
     })
-    it("Kullanici  anasayfaya basariyla erisebilmeli", () => {
+    it("The user should be able to view all products", () => {
         homePage.visitPage()
         cy.contains(user.home.homePageText).should("be.visible")
-    })
-})
-describe("Searching Test", () => {
-    const productPage = new ProductPage()
-    let user;
-    const homePage = new HomePage()
-    before(() => {
-        cy.fixture('userDatas/info').then((userInfo) => {
-            user = userInfo
-        })
-        homePage.visitPage()
-    })
-    it("Kullanici mevcut butun urunleri goruntuleyebilmeli", () => {
-
         cy.getBySel(LOCATORS.HOME_PAGE.PRODUCTS_BTN).click();
         cy.getBySel(LOCATORS.PRODUCT_PAGE.ALL_PRODUCTS_TEXT).should('be.visible')
         cy.getBySel(LOCATORS.PRODUCT_PAGE.SEARCH_BOX).type(user.productPage.productName);
