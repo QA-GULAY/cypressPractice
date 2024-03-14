506f507707d22b82a735f8bd9f26beb10ac94e41
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-14 23:07:19 +0100

    Update SubsciptionTest.cy.js

--- cypress/e2e/Test/SubsciptionTest.cy.js
+++ cypress/e2e/Test/SubsciptionTest.cy.js
@@ -1,26 +1,19 @@
 import LOCATORS from "../../support/locators";
 import HomePage from "../Page/HomePage";
-
 describe("Homepage Tests", () => {
+    
     const homePage = new HomePage()
     let user;
 
     before(() => {
-
+    
         cy.fixture('userDatas/info').then((userInfo) => {
             user = userInfo
         })
     })
-    beforeEach(() => {
+    it("The user should be able to subscribe on the home page", () => {
         homePage.visitPage()
-    })
-
-    it("Kullanici anasayfaya basariyla erisebilmeli", () => {
-
         cy.contains(user.home.homePageText).should("be.visible")
-    })
-    it("Kullanici uye olabilmeli", () => {
-
         cy.scrollTo('bottom')
         cy.getBySel(LOCATORS.HOME_PAGE.SUBSCRIPTION_TEXT).should("be.visible")
         cy.getBySel(LOCATORS.HOME_PAGE.INPUT_BOX_FOR_EMAIL).type(user.home.emailForSubscription);
